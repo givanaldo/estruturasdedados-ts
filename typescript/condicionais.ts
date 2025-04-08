bed88ecb7344e8baebe1a703515d8dbe474ed4b6
Givanaldo <givanaldo@gmail.com>
2025-04-08 08:35:54 -0300

    revisão typescript

---
+++ typescript/condicionais.ts
@@ -0,0 +1,19 @@
+/* condicionais  - if */
+let idade : number = 21
+if (idade >= 18) {
+    console.log("Você é maior de idade.")
+    console.log("Já pode ser preso.")
+}
+console.log("Terminou!")
+
+/* condicionais - if, else */
+let hora : number = 9;
+if (hora < 12) {
+    console.log("Bom dia!");
+} 
+else if (hora < 18) {
+    console.log("Boa tarde!");
+} else {
+    console.log("Boa noite!");
+}
+
