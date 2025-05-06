9646ffb5f71cd278eff4fb0bca1eef7e07322a8c
Givanaldo <givanaldo@gmail.com>
2025-05-05 23:00:27 -0300

    ordenação

--- ordenacao/algoritmos.ts
+++ ordenacao/algoritmos.ts
@@ -25,19 +25,19 @@ export function bubbleSort2<T>(arr: T[]): T[] {
 }
   
 export function selectionSort<T>(arr: T[]): T[] {
-const n = arr.length;
-for (let i = 0; i < n - 1; i++) {
-  let minIdx = i;
-  for (let j = i + 1; j < n; j++) {
-    if (arr[j] < arr[minIdx]) {
-      minIdx = j;
+  const n = arr.length;
+  for (let i = 0; i < n - 1; i++) {
+    let minIdx = i;
+    for (let j = i + 1; j < n; j++) {
+      if (arr[j] < arr[minIdx]) {
+        minIdx = j;
+      }
+    }
+    if (minIdx !== i) {
+      [arr[i], arr[minIdx]] = [arr[minIdx], arr[i]];
     }
   }
-  if (minIdx !== i) {
-    [arr[i], arr[minIdx]] = [arr[minIdx], arr[i]];
-  }
-}
-return arr;
+  return arr;
 }
 
 export function insertionSort<T>(arr: T[]): T[] {
