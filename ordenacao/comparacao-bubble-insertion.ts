6c50a1edd1115ce2addadf70e958ed5992978dcf
Givanaldo <givanaldo@gmail.com>
2025-05-05 22:56:15 -0300

    ordenação

--- ordenacao/comparacao-bubble-insertion.ts
+++ ordenacao/comparacao-bubble-insertion.ts
@@ -1,14 +1,17 @@
-import { bubbleSort1, bubbleSort2, insertionSort, selectionSort } from "./algoritmos";
+import { bubbleSort1, bubbleSort2, insertionSort, selectionSort, shellSort } from "./algoritmos";
+import { mergeSort, quickSort } from "./algoritmos";
 
 // criar vetor aleatório
 let v1 = [];
-for (let i = 0; i < 10000; i++) {
+for (let i = 0; i < 100000; i++) {
   v1.push(Math.floor(Math.random() * 10000));
 }
 let v2 = [...v1];
 let v3 = [...v1];
 let v4 = [...v1];
-
+let v5 = [...v1];
+let v6 = [...v1];
+let v7 = [...v1];
 console.log("== INICIANDO == ");
 
 let inicio = performance.now();
@@ -22,11 +25,26 @@ fim = performance.now();
 console.log(`Tempo de execução Bubble sort 2: ${fim - inicio} ms`);
 
 inicio = performance.now();
-insertionSort(v3);
+selectionSort(v3);
+fim = performance.now();
+console.log(`Tempo de execução Selection sort: ${fim - inicio} ms`);
+
+inicio = performance.now();
+insertionSort(v4);
 fim = performance.now();
 console.log(`Tempo de execução Insertion sort: ${fim - inicio} ms`);
 
 inicio = performance.now();
-selectionSort(v4);
+shellSort(v5);
 fim = performance.now();
-console.log(`Tempo de execução Selection sort: ${fim - inicio} ms`);
+console.log(`Tempo de execução Shell sort: ${fim - inicio} ms`);
+
+inicio = performance.now();
+mergeSort(v6);
+fim = performance.now();
+console.log(`Tempo de execução Merge sort: ${fim - inicio} ms`);
+
+inicio = performance.now();
+quickSort(v7);
+fim = performance.now();
+console.log(`Tempo de execução Quick sort: ${fim - inicio} ms`);
