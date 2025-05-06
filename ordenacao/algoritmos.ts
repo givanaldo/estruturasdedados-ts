60063e6eb14fad570c5fc78c1c3d9d392291e06c
Givanaldo <givanaldo@gmail.com>
2025-05-05 22:11:48 -0300

    ordenação

--- ordenacao/algoritmos.ts
+++ ordenacao/algoritmos.ts
@@ -1,4 +1,15 @@
-export function bubbleSort<T>(arr: T[]): T[] {
+export function bubbleSort1<T>(arr: T[]): T[] {
+  for (let i = 0; i < arr.length; i++) {
+      for (let j = 0; j < (arr.length - i -1); j++) {
+          if(arr[j] > arr[j+1]) {
+            [arr[j], arr[j+1]] = [arr[j+1], arr[j]];
+          }
+      }
+  }
+  return arr;
+}
+
+export function bubbleSort2<T>(arr: T[]): T[] {
   const n = arr.length;
   let swapped: boolean;
   do {
--- ordenacao/comparacao-bubble-insertion.ts
+++ ordenacao/comparacao-bubble-insertion.ts
@@ -1,26 +1,32 @@
-import { bubbleSort, insertionSort, selectionSort } from "./algoritmos";
+import { bubbleSort1, bubbleSort2, insertionSort, selectionSort } from "./algoritmos";
 
-// criar vetor aleatório com 100 elementos
+// criar vetor aleatório
 let v1 = [];
 for (let i = 0; i < 10000; i++) {
   v1.push(Math.floor(Math.random() * 10000));
 }
 let v2 = [...v1];
 let v3 = [...v1];
+let v4 = [...v1];
 
 console.log("== INICIANDO == ");
 
 let inicio = performance.now();
-const vBubble = insertionSort(v1);
+bubbleSort1(v1);
 let fim = performance.now();
-console.log(`Tempo de execução Bubble: ${fim - inicio} ms`);
+console.log(`Tempo de execução Bubble sort 1: ${fim - inicio} ms`);
 
 inicio = performance.now();
-const vInsertion = bubbleSort(v2);
+bubbleSort2(v2);
 fim = performance.now();
-console.log(`Tempo de execução Insertion: ${fim - inicio} ms`);
+console.log(`Tempo de execução Bubble sort 2: ${fim - inicio} ms`);
 
 inicio = performance.now();
-const vSelection = selectionSort(v3);
+insertionSort(v3);
 fim = performance.now();
-console.log(`Tempo de execução Selection: ${fim - inicio} ms`);
+console.log(`Tempo de execução Insertion sort: ${fim - inicio} ms`);
+
+inicio = performance.now();
+selectionSort(v4);
+fim = performance.now();
+console.log(`Tempo de execução Selection sort: ${fim - inicio} ms`);
