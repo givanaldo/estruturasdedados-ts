b350649c3e0e7686cfa29e5b6261925f16c30884
Givanaldo <givanaldo@gmail.com>
2025-06-23 10:31:12 -0300

    hash e dict

--- hash/hash_functions.ts
+++ hash/hash_functions.ts
@@ -7,6 +7,7 @@ console.log(hash(63, 10));  // retorna 3
 function hash_str(chave: string, tamanho: number): number {
     let hash = 0;
     for (let i = 0; i < chave.length; i++) {
+        console.log(`${chave[i]}: ${chave.charCodeAt(i)}`);
         hash += chave.charCodeAt(i);
     }
     return hash % tamanho;
--- mapa/produtos_map.ts
+++ mapa/produtos_map.ts
@@ -3,8 +3,10 @@ const mapaProdutos = new Map<number, string>();
 mapaProdutos.set(1, "Teclado");
 mapaProdutos.set(2, "Mouse");
 mapaProdutos.set(3, "Monitor");
+mapaProdutos.set(4, "Pendrive");
 
-console.log(mapaProdutos.get(2));
+for (let [chave, valor] of mapaProdutos.entries())
+    console.log(`[${chave}] ${valor}`);
 
 mapaProdutos.forEach((valor, chave) => {
     console.log(`[${chave}] ${valor}`);
