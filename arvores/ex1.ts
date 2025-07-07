886b08b27a1fc4a8b1c921a79720a0b16e56523e
Givanaldo <givanaldo@gmail.com>
2025-07-07 10:31:55 -0300

    ARVORES

--- arvores/ex1.ts
+++ arvores/ex1.ts
@@ -5,13 +5,12 @@ const root = new TreeNode("A");
 const nodeB = new TreeNode("B");
 const nodeC = new TreeNode("C");
 const nodeD = new TreeNode("D");
+const nodeE = new TreeNode("E");
+const nodeF = new TreeNode("F");
 
 root.addChild(nodeB);
 root.addChild(nodeC);
 root.addChild(nodeD);
-
-const nodeE = new TreeNode("E");
-const nodeF = new TreeNode("F");
 
 nodeC.addChild(nodeE);
 nodeC.addChild(nodeF);
--- arvores/ex2.ts
+++ arvores/ex2.ts
@@ -1,17 +1,22 @@
 import { TreeNode } from "./TreeNode";
 
 const root = new TreeNode("Tecnologia");
-
 const web = new TreeNode("Web");
 const mobile = new TreeNode("Mobile");
 
 root.addChild(web);
 root.addChild(mobile);
 
-web.addChild(new TreeNode("Frontend"));
+const frontend = new TreeNode("Frontend")
+
+web.addChild(frontend);
 web.addChild(new TreeNode("Backend"));
 
 mobile.addChild(new TreeNode("iOS"));
 mobile.addChild(new TreeNode("Android"));
 
-root.printTree();
+frontend.addChild(new TreeNode("HTML"));
+frontend.addChild(new TreeNode("CSS"));
+frontend.addChild(new TreeNode("Javascript"));
+
+frontend.printTree();
