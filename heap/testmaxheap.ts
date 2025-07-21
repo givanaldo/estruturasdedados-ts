025c55e9f45147d42ae432b61189c94f91aced1d
Givanaldo <givanaldo@gmail.com>
2025-07-21 10:20:59 -0300

    heap

--- heap/testmaxheap.ts
+++ heap/testmaxheap.ts
@@ -10,5 +10,8 @@ heap.insert(25);
 
 heap.print(); // Deve mostrar algo como: [70, 30, 5, 10, 25]
 
-console.log(heap.extractMax()); // 70
-heap.print(); // Atualiza o heap após remoção
+heap.insert(90);
+heap.print(); 
+
+console.log(heap.extractMax());
+heap.print(); 
